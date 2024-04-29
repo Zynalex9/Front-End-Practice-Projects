4b7e76f4b28f2a728d81dc68fe06c400f893f262
Zynalex9 <alzn952@gmail.com>
2024-04-29 18:31:59 +0500

    Added Functionality

--- RandomChoice/script.js
+++ RandomChoice/script.js
@@ -1,45 +1,69 @@
-const tagsEl = document.getElementById("tags");
-const textarea = document.getElementById("textarea");
-// textarea.focus()
-textarea.addEventListener("keydown", (e) => {
-  createTags(e.target.value);
-  if (e.key === "Enter") {
-    setTimeout(() => {
-      e.target.value = "";
-    }, 10);
-    RandomSelect();
-  }
-});
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
+})
+
 function createTags(input) {
-  let tags = input
-    .split(",")
-    .filter((tag) => tag.trim() !== "")
-    .map((tag) => tag.trim());
-  tagsEl.innerHTML = "";
-  tags.forEach((tag) => {
-    const tagEl = document.createElement("span");
-    tagEl.classList.add("tag");
-    tagEl.innerText = tag;
-    tagsEl.appendChild(tagEl);
-  });
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
 }
-function RandomSelect() {
-  const time = 30;
-  const randomTag = pickRandomTag();
-  const interval = setInterval(() => {
-    highlightedTag(randomTag);
-    setTimeout(() => {
-      unhighlightedTag(randomTag);
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+	
+	if (randomTag !== undefined) {
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+	}
     }, 100);
-  }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
 }
+
 function pickRandomTag() {
-  let rTags = document.querySelectorAll(".tag");
-  return rTags[Math.floor(Math.random() * rTags.length)]; // Added parentheses after Math.random
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
 }
-function highlightedTag(tag) {
-  tag.classList.add("highligted");
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
 }
-function unhighlightedTag(tag) {
-  tag.classList.remove("highligted");
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
