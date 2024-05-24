a05020903e979e45585e47df144d8e3a9741f13d
Zynalex9 <alzn952@gmail.com>
2024-05-24 18:02:02 +0500

    Axios request

---
+++ GitHub Profile/script.js
@@ -0,0 +1,22 @@
+const search = document.getElementById("seach");
+const form = document.getElementById("form");
+const APIURL = "https://api.github.com/users/";
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username);
+    console.log(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = "";
+  }
+});
